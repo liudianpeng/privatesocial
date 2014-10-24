d96ca080b9fb605574d25f777bf1b0ef100b0f51
Haulyn Jason <iam@saharabear.com>
2014-10-24 17:21:43 +0800

    now all pictures are private

--- src/Slackiss/Bundle/SocialBundle/Controller/DefaultController.php
+++ src/Slackiss/Bundle/SocialBundle/Controller/DefaultController.php
@@ -7,6 +7,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
 class DefaultController extends Controller
 {
     /**
@@ -23,4 +27,36 @@ class DefaultController extends Controller
             return ['nav_active'=>'welcome'];
         }
     }
+
+    /**
+     *
+     * @Route("/social/member/{id}/pic", name="social_member_avatar")
+     * @Method("GET")
+     */
+    public function picAction(Request $request,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('SlackissSocialBundle:MemberProfile');
+        $entity = $repo->find($id);
+
+        if(!$entity){
+            throw $this->createNotFoundException('没有这个图片');
+        }
+        $response = new Response();
+        $file = $entity->getAvatarAttach();
+        $response->headers->set('Content-Type', $file->getMimeType());
+        $response->headers->set('Content-Disposition', 'attachment;filename="'.$this->getFileName(md5($entity->getId()), $file->guessExtension()).'"');
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        $response->setStatusCode(200);
+        $response->setContent(file_get_contents($file));
+        return $response;
+    }
+
+    protected function getFileName($name, $mimeType)
+    {
+        return $name.'.'.$mimeType;
+    }
+
 }
--- src/Slackiss/Bundle/SocialBundle/Controller/ResourceController.php
+++ src/Slackiss/Bundle/SocialBundle/Controller/ResourceController.php
@@ -3,6 +3,7 @@
 namespace Slackiss\Bundle\SocialBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -61,4 +62,37 @@ class ResourceController extends Controller
             'entity' => $entity,
         );
     }
+
+    /**
+     *
+     * @Route("/{id}/pic", name="social_resource_pic_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function picAction(Request $request,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('SlackissSocialBundle:Resource');
+        $entity = $repo->find($id);
+
+        if(!$entity){
+            throw $this->createNotFoundException('没有这个图片');
+        }
+        $response = new Response();
+        $file = $entity->getFaceAttach();
+        $response->headers->set('Content-Type', $file->getMimeType());
+        $response->headers->set('Content-Disposition', 'attachment;filename="'.$this->getFileName(md5($entity->getId()), $file->guessExtension()).'"');
+        $response->headers->set('Content-Transfer-Encoding', 'binary');
+        $response->headers->set('Pragma', 'no-cache');
+        $response->headers->set('Expires', '0');
+        $response->setStatusCode(200);
+        $response->setContent(file_get_contents($file));
+        return $response;
+    }
+
+    protected function getFileName($name, $mimeType)
+    {
+        return $name.'.'.$mimeType;
+    }
+
 }
